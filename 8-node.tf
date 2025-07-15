aa353e765b0086f61f4a53837189686fb75a05cc
Dharmendra Bhagat <dhab@tdcnet.dk>
2025-07-15 22:26:32 +0530

    Working Till NodeGroups

---
+++ 8-node.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_role" "example" {
+  name = "${local.eks_cluster_name}-${local.environment}eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = "${local.eks_cluster_name}-${local.environment}"
+  node_group_name = "${local.eks_cluster_name}-${local.environment}-nodegroup"
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids = [
+      aws_subnet.private-zone1.id,
+      aws_subnet.private-zone2.id
+    ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "name" {
-  value = aws_vpc.main.id
-}
+#output "name" {
+#  value = aws_vpc.main.id
+#}
